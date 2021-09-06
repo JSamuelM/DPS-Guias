b1185c6943931dc2a4bd0e0e737339c4b52e93f0
Samuel Mena <samuel23mena@gmail.com>
2021-09-05 18:29:28 -0600

    Cambios src en discusion

--- cars/App.js
+++ cars/App.js
@@ -1,111 +1,73 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
+  View,
+  FlatList,
   StyleSheet,
   Text,
-  useColorScheme,
-  View,
+  StatusBar,
+  Image,
 } from 'react-native';
+const DATA = [
+  {
+    id: '1',
+    title: 'Toyota',
+    src: require('./src/img/toyota.jpg'),
+  },
+  {
+    id: '2',
+    title: 'Mazda',
+    src: require('./src/img/mazda.jpg'),
+  },
+  {
+    id: '3',
+    title: 'Mitsubishi',
+    src: require('./src/img/mitsubishi.jpg'),
+  },
+];
+const Item = ({title, img}) => (
+  <View style={styles.item}>
+    <Text style={styles.title}>{title}</Text>
+    <Image style={styles.img} source={img} />
+  </View>
+);
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const App = () => {
+  const renderItem = ({item}) => <Item title={item.title} img={item.src} />;
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={item => item.id}
+      />
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    marginTop: StatusBar.currentHeight || 0,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  item: {
+    backgroundColor: '#f9c2ff',
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
+    alignItems: 'center',
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  title: {
+    fontSize: 32,
   },
-  highlight: {
-    fontWeight: '700',
+  img: {
+    width: 200,
+    height: 125,
+    borderWidth: 2,
+    borderColor: '#d35647',
+    resizeMode: 'contain',
+    margin: 8,
   },
 });
 
