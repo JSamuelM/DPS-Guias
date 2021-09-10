f7c628db0bd4e8fc98ed9cb455b904af05420341
Samuel Mena <samuel23mena@gmail.com>
2021-09-09 20:43:28 -0600

    :sparkles: Agregando procedimiento de la guia 04

--- cotizador-prestamos/App.js
+++ cotizador-prestamos/App.js
@@ -1,21 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { SafeAreaView, StyleSheet, View, Text, StatusBar } from "react-native";
+import Form from "./src/components/Form";
+import Result from "./src/components/Result";
+import Footer from "./src/components/Footer";
+
+import colors from "./src/utils/colors";
 
 export default function App() {
+  const [capital, setCapital] = useState(null);
+  const [interest, setInterest] = useState(null);
+  const [months, setMonths] = useState(null);
+  const [total, setTotal] = useState(null);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    if (capital && interest && months) {
+      calculate();
+    } else {
+      reset();
+    }
+  }, [capital, interest, months]);
+
+  const calculate = () => {
+    reset();
+    if (!capital) {
+      setErrorMessage("Añade la cantidad que quieres solicitar");
+    } else if (!interest) {
+      setErrorMessage("Añade el interes del prestamo");
+    } else if (!months) {
+      setErrorMessage("Selecciona los meses a pagar");
+    } else {
+      const i = interest / 100;
+      const fee = capital / ((1 - Math.pow(i + 1, -months)) / i);
+      setTotal({
+        monthlyFree: fee.toFixed(2).replace(".", ","),
+        totalPayable: (fee * months).toFixed(2).replace(".", ","),
+      });
+    }
+  };
+
+  const reset = () => {
+    setErrorMessage("");
+    setTotal(null);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar barStyle="light-content" />
+      <SafeAreaView style={styles.Header}>
+        <Text style={styles.HeadApp}>Cotizador de Prestamos</Text>
+        <Form
+          setCapital={setCapital}
+          setInterest={setInterest}
+          setMonths={setMonths}
+        />
+      </SafeAreaView>
+      <Result
+        capital={capital}
+        interest={interest}
+        months={months}
+        total={total}
+        errorMessage={errorMessage}
+      />
+      <Footer calculate={calculate} />
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  Header: {
+    backgroundColor: colors.PRIMARY_COLOR,
+    height: 200,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    alignItems: "center",
+  },
+  HeadApp: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: "#fff",
+    marginTop: 15,
   },
 });
