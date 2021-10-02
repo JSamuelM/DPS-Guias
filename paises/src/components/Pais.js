f38001edaa6388cf02ef8fd63e7b92e19acd2a37
Samuel Mena <samuel23mena@gmail.com>
2021-10-01 18:34:37 -0600

    :lipstick: Agregando discusion de guia 8

--- paises/src/components/Pais.js
+++ paises/src/components/Pais.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {Text, View} from 'react-native';
+import {Image, Text, View} from 'react-native';
 import {Card} from 'react-native-elements';
 
 const Pais = ({resultado}) => {
@@ -8,6 +8,7 @@ const Pais = ({resultado}) => {
   const [capital, setCapital] = useState();
   const [region, setRegion] = useState();
   const [lengua, setLengua] = useState([]);
+  const [superficie, setSuperficie] = useState();
 
   useEffect(() => {
     setInfo(resultado);
@@ -16,6 +17,7 @@ const Pais = ({resultado}) => {
       setNombre(e.nome.abreviado);
       setCapital(e.governo.capital.nome);
       setRegion(e.localizacao.regiao.nome);
+      setSuperficie(e.area.total * 1000);
       Object.values(e.linguas).map(l => {
         lengua.push(l.nome);
       });
@@ -27,9 +29,15 @@ const Pais = ({resultado}) => {
       <Card.Title>{nombre}</Card.Title>
       <Card.Divider />
       <View style={{justifyContent: 'center'}}>
+        <Image 
+          style={}
+        />
         <Text>Capital: {capital}</Text>
         <Text>Region: {region}</Text>
         <Text>Lengua: {lengua.toString()}</Text>
+        <Text>
+          Superficie Total: {superficie > 0 ? superficie : 0} Metros Cuadrados
+        </Text>
       </View>
     </Card>
   );
